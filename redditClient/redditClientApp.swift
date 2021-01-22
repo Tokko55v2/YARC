c06adfdaf886bb5895318b206f4eefd3517d7f44
Michael <michael@kroneder.net>
2021-01-22 20:56:52 +0100

    Initial Commit

---
+++ redditClient/redditClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  redditClientApp.swift
+//  redditClient
+//
+//  Created by Michael Kroneder on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct redditClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
