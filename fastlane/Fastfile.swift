34b4ed7ed6ee5ea09238edd75f486ebf7018d5f5
tokko55v2 <michael@kroneder.net>
2021-01-23 00:28:06 +0100

    edit workflow and fastlane

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -9,11 +9,7 @@
 import Foundation
 
 class Fastfile: LaneFile {
-	func customLane() {
-	desc("Description of what the lane does")
-		// add actions here: https://docs.fastlane.tools/actions
-	}
-    
+    // add actions here: https://docs.fastlane.tools/actions
     func swiftLintLane() {
            desc("Run SwiftLint")
            swiftlint(configFile: ".swiftlint.yml",
@@ -22,37 +18,18 @@ class Fastfile: LaneFile {
                      raiseIfSwiftlintError: true,
                      executable: "Pods/SwiftLint/swiftlint"
            )
-       }
+    }
     
-    func buildLane() {
-           desc("Build for testing")
-           scan(workspace: "reddit-client-iOS.xcworkspace",
-                derivedDataPath: "derivedData",
-                buildForTesting: true,
-                xcargs: "CI=true")
-       }
+    func cocoapodsLane() {
+        cocoapods(
+          clean_install: true,
+          podfile: "./Podfile"
+        )
+    }
     
-    func unitTestLane() {
-            desc("Run unit tests")
-            scan(workspace: "reddit-client-iOS.xcworkspace",
-                 onlyTesting: ["reddit-client-CoreTests"],
-                 derivedDataPath: "derivedData",
-                 testWithoutBuilding: true)
-        }
-    
-    func integrationTestLane() {
-            desc("Run integration tests")
-            scan(workspace: "reddit-client-iOS.xcworkspace",
-                 onlyTesting: ["reddit-client-iOSTests"],
-                 derivedDataPath: "derivedData",
-                 testWithoutBuilding: true)
-        }
-    
-    func uiTestLane() {
-           desc("Run UI tests")
-           scan(workspace: "reddit-client-iOS.xcworkspace",
-                onlyTesting: "reddit-client-iOSUITests",
-                derivedDataPath: "derivedData",
-                testWithoutBuilding: true)
-       }
+    func xcTestLane() {
+        xctest(
+          destination: "name=iPhone 11,OS=14.0"
+        )
+    }
 }
