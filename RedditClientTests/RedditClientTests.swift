9a4697621deea98fbb40a73e3277e6d29bca959d
tokko55v2 <michael@kroneder.net>
2021-01-23 02:11:33 +0100

    rename stuff 2

--- RedditClientTests/RedditClientTests.swift
+++ RedditClientTests/RedditClientTests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import redditClient
+@testable import RedditClient
 
-class redditClientTests: XCTestCase {
+class RedditClientTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
