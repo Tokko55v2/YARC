a6d2ee32391c955d84f03eb33a236a5cec064cc0
tokko55v2 <michael@kroneder.net>
2021-01-27 23:42:36 +0100

    fetch posts

--- RedditClient/Posts/ListViewModel.swift
+++ RedditClient/Posts/ListViewModel.swift
@@ -5,4 +5,27 @@
 //  Created by Michael Kroneder on 26/01/2021.
 //
 
-import Foundation
+import Alamofire
+import Combine
+import SwiftUI
+
+class ListViewModel: ObservableObject {
+    private var dataService: DataService?
+
+    @Published var posts = Posts()
+
+    init() {
+        dataService = DataService()
+        fetchPosts()
+    }
+
+    func fetchPosts() {
+        dataService?.requestFetchPosts(completion: { posts, error in
+            if let error = error {
+                print("\(error)")
+                return
+            }
+            self.posts = posts ?? Posts()
+        })
+    }
+}
