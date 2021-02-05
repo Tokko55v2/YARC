731e9e6cdf9ccaacc8cce8511bb8a15de1da8c57
tokko55v2 <michael@kroneder.net>
2021-02-05 22:03:27 +0100

    Remove-OAuth2

--- RedditClient/Entropy/Entropy.swift
+++ RedditClient/Entropy/Entropy.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import WebKit
 
-struct LoginView: View {
+struct Entropy: View {
     @State var moveToListView: Bool = false
     @State var hasProfile: Bool = false
 
@@ -61,6 +61,6 @@ private struct BackgroundColor: View {
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        Entropy()
     }
 }
