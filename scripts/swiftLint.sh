cabbdfb309d23536288670b63e8cb3008c8f6dca
Michael Kroneder <michael@kroneder.net>
2021-05-13 09:52:17 +0200

    XCodeGen - swiftlint config

--- scripts/swiftLint.sh
+++ scripts/swiftLint.sh
@@ -1,4 +1,4 @@
 # Type a script or drag a script file from your workspace to insert its path.
 if [ -z "$CI" ]; then
-    $SRCROOT/../Pods/SwiftLint/swiftlint
+    $SRCROOT/../Pods/SwiftLint/swiftlint --config .swiftlint.yml
 fi
